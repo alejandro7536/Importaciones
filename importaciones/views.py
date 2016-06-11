824b127c0929630c014bd2edd38e285612bcf59d
alejandro <alejandroalas75@hotmail.com>
2016-06-11 13:15:40 -0600

    proyecto hdp maquetacion

---
+++ importaciones/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def inicio(request):
+    return render(request, 'Index.html')
+
+def login(request):
+    return render(request, 'login.html')
+def gestion(request):
+    return render(request, 'data.html')

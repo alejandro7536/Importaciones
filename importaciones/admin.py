1c30821f962d59134fdbd9f3fec25ad3ff2a7231
William <soywaco@gmail.com>
2016-06-16 21:15:01 -0600

    Se modifico el panel de administracion para desplegar la informacion de una forma mas clara.

--- importaciones/admin.py
+++ importaciones/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from importaciones.models import Categoria, Producto, Estadistica
 
 # Register your models here.
-admin.site.register(Categoria)
-admin.site.register(Producto)
-admin.site.register(Estadistica)
+
+class CategoriaAdmin(admin.ModelAdmin):
+	list_display = ('nombre_categoria',)
+
+class ProductoAdmin(admin.ModelAdmin):
+	list_display = ('codigo', 'nombre_producto', 'categoria')
+
+class EstadisticaAdmin(admin.ModelAdmin):
+	list_display = ('producto', 'valor', 'cantidad', 'mes', 'anho')
+
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Estadistica, EstadisticaAdmin)
